e22aa67d85d0083491fe295fafc0b3d79421298a
ViCt0r99 <wiktor.jan999@gmail.com>
2023-03-31 15:38:07 +0200

    feat: debug functions, add utils file inside test directory

--- tests/utils.py
+++ tests/utils.py
@@ -1,38 +1,13 @@
+from bw_visualization.sankertainpy import plot
+
 import bw2data as bd
-import bw2calc as bc
 import bw2io as bi
-import numpy as np
-from tqdm import tqdm
-from pathlib import Path
-import pandas as pd
-import json
-
-
-bd.projects.set_current("to_dataframe USEEIO")
-
-datadir = Path("data")
-datadir.mkdir(exist_ok=True)
-
+bd.projects.set_current("graphics-fixture")
 bi.useeio11()
 
-df = bd.Database("USEEIO-1.1").nodes_to_dataframe()
-
-
-def safe_joiner(x):
-    try:
-        if not x or np.isnan(x):
-            return None
-    except TypeError:
-        pass
-    return "::".join(x)
-
-
-df['categories'] = df['categories'].apply(safe_joiner)
-
-
-def safe_converter(x):
-    try:
-        return str(int(x))
-    except:
-        return None
+eidb = bd.Database('USEEIO-1.1').random({"name": "Cutlery and handtools; at manufacturer",
+                                         'code': '1fb0a77a-d49c-3557-810c-a4db0e73bab6'})
+method = bd.methods.random()
+result = plot(eidb, method)
+result.show()
 

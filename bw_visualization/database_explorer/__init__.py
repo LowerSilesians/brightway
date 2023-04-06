879a771a9ae9fbf21f349f0d6a1b2f87be283578
HAkhavan71 <hakh.27@gmail.com>
2023-04-06 14:48:14 +0330

    fix: add strict arg to database explorer plot

--- bw_visualization/database_explorer/__init__.py
+++ bw_visualization/database_explorer/__init__.py
@@ -11,9 +11,11 @@ def plot(database,
          amount=1,
          location="",
          unit="",
-         list_act_input=None
+         list_act_input=None,
+         strict=False,
          ):
     list_act = ListAct(database, name, methods_ef, methods_cc, location, unit, list_act_input)
+    list_act.search(strict=strict)
     app = list_act.dashboard(i, method_cc, cutoff, amount)
     app.run_server(port=8058, debug=True, use_reloader=False)
 

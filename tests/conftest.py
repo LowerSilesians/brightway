1f9d0c8e7c5950df2adf4487610c140750f3c353
Chris Mutel <cmutel@gmail.com>
2023-04-13 10:54:18 +0200

    Fix installation of text fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,38 @@
 
 import pytest
 import bw2data as bd
-import bw2io as bi
 
-PROJECT_NAME = 'test'
-BACKUP_PATH = ''
+import bw2io as bi
+from pathlib import Path
+import requests
+
+
+FIXTURES_DIR = Path(__file__).parent.absolute() / "fixtures"
+FIXTURES_DIR.mkdir(exist_ok=True)
+USEEIO_FILENAME = "useeio.tar.gz"
+USEEIO_FIXTURE = FIXTURES_DIR / USEEIO_FILENAME
+PROJECT_NAME = 'USEEIO-1.1-noproducts'
 
 
 def pytest_sessionstart(session):
-    global BACKUP_PATH
-    bd.projects.set_current(PROJECT_NAME)
-    bi.useeio11(collapse_products=True, prune=True)
-    BACKUP_PATH = bi.backup_project_directory(PROJECT_NAME)  # backup_project_directory doesn't return file path and cant be used
+    if not USEEIO_FIXTURE.exists():
+        URL = "https://files.brightway.dev/" + USEEIO_FILENAME
+        request = requests.get(URL, stream=True)
+        if request.status_code != 200:
+            raise NotFound(
+                "URL {} returns status code {}.".format(URL, request.status_code)
+            )
+        download = request.raw
+        chunk = 128 * 1024
+        with open(USEEIO_FIXTURE, "wb") as f:
+            while True:
+                segment = download.read(chunk)
+                if not segment:
+                    break
+                f.write(segment)
 
 
 @pytest.fixture
 def restore_database():
-    bi.restore_project_directory(BACKUP_PATH)
+    bi.restore_project_directory(USEEIO_FIXTURE)
     bd.projects.set_current(PROJECT_NAME)
